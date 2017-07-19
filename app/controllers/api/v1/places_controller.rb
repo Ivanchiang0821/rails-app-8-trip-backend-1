53c136e475438c40e7d161172a55a2583014342e
Ivanchiang0821 <ivanchiang.tw@gmail.com>
2017-07-19 15:03:16 +0800

    Add files via upload

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -87,8 +87,8 @@ module Api
 		  		r["eng_name"] = english_results[i]
 		  	end
 
-		  	tripadvisor_result = get_tripadvisor_info(params[:str])
-		  	@response["tripadvisor"] = tripadvisor_result
+//		  	tripadvisor_result = get_tripadvisor_info(params[:str])
+//		  	@response["tripadvisor"] = tripadvisor_result
 
 		  	k = KeywordCount.find_by(keyword: params[:str], option: params[:opt])
 		  	if k
@@ -97,7 +97,7 @@ module Api
 		  		KeywordCount.create(keyword: params[:str], option: params[:opt], 
 		  			                  count: 1, debug: @debug, 
 		  			                  r_count: @response["results"].count, 
-		  			                  ta_count: @response["tripadvisor"].count, 
+		  			                  ta_count: 0, //@response["tripadvisor"].count, 
 		  			                  autocomplete: @place ? true : false)
 		  	end		  	
 		  end
